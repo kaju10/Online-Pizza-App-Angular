b9b53d32d7ada82f327dbcb9870671612db7a439
Saptarshi Roy <46836617+saptarshi-18-15-25@users.noreply.github.com>
2021-08-25 12:57:58 +0530

    Ts file added

--- src/app/components/adminhomepage/pizzacrud/pizzacrud.component.ts
+++ src/app/components/adminhomepage/pizzacrud/pizzacrud.component.ts
@@ -7,9 +7,133 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzacrudComponent implements OnInit {
 
-  constructor() { }
+ pizza: Pizza = new Pizza(0,"","","","",0,"");
+  pizzas : any; 
+  closeResult!: string;
+  private deleteId!: any;
+  id!: number;
+  editForm!: FormGroup;
+
+  constructor(
+    private service:PizzaserviceService, 
+   private modalService: NgbModal,
+   private fb:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.getPizzas();
+
+    this.editForm = this.fb.group(
+      {
+        pizzaId : [0],
+        pizzaType :[''],
+        pizzaName :[''],
+        pizzaSize :[''],
+        pizzaDescription:[''],
+        pizzaCost :[0],
+        pizzaImageUrl:['']
+      }
+    )
+  }
+
+  //modal pop-up methods
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  //Create -----------------------------------------------------------------
+  onSubmit(f: NgForm) {
+
+    this.service.addPizzaService(this.pizza)
+       .subscribe((result) => {
+         this.ngOnInit(); //reload the table
+       });
+     this.modalService.dismissAll(); //dismiss the modal
+   }
+
+  //Read (All) ---------------------------------------------------------------
+  getPizzas(){
+    this.service.getAllPizzaService()
+    .subscribe(
+      response => {
+        console.log(response);
+        this.pizzas = response;
+      }
+    );
+  }
+
+  //Read (Single)
+  public findPizzaById()
+ {
+  this.service.getPizzaService(this.id)
+  .subscribe(
+    response => {
+      console.log(response);
+      this.pizzas = response;
+    }
+  );
+ }
+
+ //Update --------------------------------------------------------------------
+ openEdit(targetModal: any, editPizza: Pizza) {
+  this.modalService.open(targetModal, {
+    backdrop: 'static',
+    size: 'lg'
+  });
+  this.editForm.patchValue( {
+    pizzaId : editPizza.pizzaId,
+    pizzaType : editPizza.pizzaType,
+    pizzaName : editPizza.pizzaName,
+    pizzaSize : editPizza.pizzaSize,
+    pizzaDescription : editPizza.pizzaDescription,
+    pizzaCost : editPizza.pizzaCost,
+    pizzaImageUrl : editPizza.pizzaImageUrl
+  });
+ }
+ ePizza : any;
+
+ onSave() {
+  this.ePizza= this.editForm.value;
+ this.service.updatePizzaService(this.editForm.value.pizzaId,this.ePizza)
+    .subscribe((results) => {
+      this.ngOnInit();
+      this.modalService.dismissAll();
+    });
+  }
+
+  //Delete ------------------------------------------------------------------------
+  openDelete(targetModal: any, delpizza: Pizza) {
+    this.deleteId = delpizza.pizzaId;
+    this.modalService.open(targetModal, {
+      backdrop: 'static',
+      size: 'lg'
+    });
+  }
+
+  onDelete() {
+    this.service.deletePizzaService(this.deleteId)
+      .subscribe((results) => {
+        this.ngOnInit();
+      this.modalService.dismissAll();
+        
+      });
+      
+      
   }
 
 }
