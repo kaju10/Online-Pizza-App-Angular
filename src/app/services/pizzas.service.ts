65952c30f6b4a07101897163fbbe1658d9f0e524
Saptarshi Roy <46836617+saptarshi-18-15-25@users.noreply.github.com>
2021-08-25 12:55:52 +0530

    Pizza Service Updated
    _httpClient over all place

--- src/app/services/pizzas.service.ts
+++ src/app/services/pizzas.service.ts
@@ -10,16 +10,46 @@ import { viewFilteredPizzaUrl, viewAllPizzaUrl } from '../config';
 })
 export class PizzasService {
 
-  constructor(private httpRequests: HttpClient) { }
+  constructor(private _httpClient: HttpClient) { }
 
 
 
     public getAllPizzas(): Observable<Pizza[]>{
-      return this.httpRequests.get<Pizza[]>(viewAllPizzaUrl);
+      return this._httpClient.get<Pizza[]>(viewAllPizzaUrl);
     }
 
     public getFilteredPizzas(value1 : number , value2 : number): Observable<Pizza[]>{
-      return this.httpRequests.get<Pizza[]>(viewFilteredPizzaUrl + '/' + value1 + '/' + value2);
+      return this._httpClient.get<Pizza[]>(viewFilteredPizzaUrl + '/' + value1 + '/' + value2);
     }
+  
+  public addPizzaService(pizza:Pizza): Observable<Pizza>
+  {
+       return this._httpClient.post<Pizza>("http://localhost:9090/addpizza",pizza);
+  }
+
+  public getAllPizzaService()
+  {
+    return this._httpClient.get<any>("http://localhost:9090/viewpizza");
+  }
+
+  public getPizzaService(id:any)
+  {
+    return this._httpClient.get("http://localhost:9090/viewpizza/"+id);
+  }
+
+  public getPizzaByTypeService(pizzaType:any)
+  {
+    return this._httpClient.get("http://localhost:9090/viewpizza/type/"+pizzaType);
+  }
+
+  public updatePizzaService(id:any, pizza:Pizza)
+  {
+    return this._httpClient.put<Pizza>('http://localhost:9090/updatepizza',pizza);
+  }
+
+  public deletePizzaService(pizzaId:any)
+  {
+    return this._httpClient.delete<string>('http://localhost:9090/deletepizza/' + pizzaId,{responseType:'text' as 'json'});
+  }
 
 }
